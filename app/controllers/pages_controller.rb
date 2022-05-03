292e0260d76cd96ba5d2bc6e7863b02b6e2cb18a
Babin Bohora <bohorababin1@gmail.com>
2022-05-03 19:43:51 +0900

    subscribed method and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,23 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def subscribe
+    price_id = params[:priceId]
+
+    @session = Stripe::Checkout::Session.create({
+      success_url: root_url,
+    cancel_url: subscribe_url,
+      mode: 'subscription',
+      line_items: [{
+        # For metered billing, do not pass quantity
+        quantity: 1,
+        price: price_id,
+      }],
+    })
+  end
+
+  def payment
+
+  end
 end
