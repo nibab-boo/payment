afec79615ccd075f248b6a72e21ac095c75f273f
Babin Bohora <bohorababin1@gmail.com>
2022-05-03 19:42:59 +0900

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'paytohome', to: 'pages#home'
-  get 'checkout', to: 'checkouts#show'
+  post 'subscribe', to: 'pages#subscribe'
+  get 'subscribe', to: 'pages#payment'
   get 'billing', to: 'billing#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
