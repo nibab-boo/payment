562d8e7c979b1423c075f392da6f7bc912b472c0
Babin Bohora <bohorababin1@gmail.com>
2022-05-04 23:43:22 +0900

    webhooks controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   root to: 'pages#home'
   get 'paytohome', to: 'pages#home'
   post 'subscribe', to: 'pages#subscribe'
-  get 'payment', to: 'pages#payment'
-  get 'billing', to: 'billing#show'
 
   get 'pricing', to: 'static#pricing'
 
-  mount StripeEvent::Engine, at: '/stripe-webhooks'
+  namespace :purchase do
+    resources :checkouts
+  end
+
+  get 'success', to: 'purchase/checkouts#success'
+
+  resources :subscriptions, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
